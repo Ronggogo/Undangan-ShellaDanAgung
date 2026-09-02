9342147a4168bb46d87186b5f5548081f27b3a66
Ronggogo <m.dzaki22ti@mahasiswa.pcr.ac.id>
2026-09-02 23:49:18 +0700

    first commit

---
+++ fiveserver.config.js
@@ -0,0 +1,6 @@
+// Five Server configuration
+module.exports = {
+  host: '0.0.0.0',       // Mengizinkan koneksi dari semua perangkat dalam satu jaringan WiFi/LAN
+  useLocalhost: false,   // Menampilkan alamat IP jaringan lokal
+  cors: true,            // Mengaktifkan CORS agar asset & fonts dapat dimuat tanpa kendala
+};
